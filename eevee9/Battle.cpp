b5dbc61810e07650cf9e29d3ae8c7354a8ebc678
EmilieGonin <emilie.yuu@gmail.com>
2022-11-24 12:38:47 +0100

    add loot and init functions for Battle class

--- eevee9/Battle.cpp
+++ eevee9/Battle.cpp
@@ -1,5 +1,29 @@
 #include "Battle.h"
+#include <random>
+#include <iostream>
+
+Battle::Battle(Eevee* eevee, Enemy* enemy) {
+	this->_eevee = eevee;
+	this->_enemy = enemy;
+	this->_win = false;
+	this->_loose = false;
+	this->_choice = 0;
+}
+
+Battle::~Battle() {};
 
 void Battle::loot() {
-	//
+	srand(time(NULL));
+	int loot = rand() % 6;
+	std::cout << "Getting loot : " << loot;
+
+	if (loot > 3) {
+		this->_eevee->addLoot(loot);
+	}
+}
+
+void Battle::turn() {
+	while (this->_choice == 0) {
+		//afficher menu
+	}
 }
--- eevee9/Battle.h
+++ eevee9/Battle.h
@@ -1,11 +1,19 @@
 #pragma once
+#include "Eevee.h"
+#include "Enemy.h"
+
 class Battle
 {
 protected:
-	bool win;
-	bool loose;
-	int choice;
+	bool _win;
+	bool _loose;
+	int _choice;
+	Eevee* _eevee;
+	Enemy* _enemy;
 public:
+	Battle(Eevee*, Enemy*);
+	~Battle();
 	void loot(); // Choose a random loot on a scale of 0 to 6, 0-3 = nothing
+	void turn();
 };
 
