2a5444129b386b9ec9b3e9ef08e17a90573e5934
EmilieGonin <emilie.yuu@gmail.com>
2022-12-01 15:12:48 +0100

    add saving fonctionnality

--- eevee9/include/Database.h
+++ eevee9/include/Database.h
@@ -10,8 +10,9 @@ std::vector<std::vector<std::string>> dataSQL(sqlite3*, const char*);
 void SQL(sqlite3*, const char*);
 std::vector<std::string> getEnemy(sqlite3*);
 std::vector<std::string> getSave(sqlite3*);
-//1[HP int], 2[Waterstone], 3[Thunderstone], 4[Firestone], 5[Map], 6[x], 7[y]
-void setSave(sqlite3*, int, int, int, int, int, int, int);
+//1[HP int], 2[Waterstone], 3[Thunderstone], 4[Firestone], 5[Map],
+//6[x], 7[y], 8[Orientation int]
+void setSave(sqlite3*, int, int, int, int, int, int, int, int);
 std::vector<std::string> getPlayer(sqlite3*);
 void createDatabase(sqlite3*);
 sqlite3* getDatabase();
